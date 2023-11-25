eda222ec0a5e71c648a038e4f854d0204cb075c7
Nitesh000 <ntudu040@gmail.com>
2023-11-25 09:16:51 +0530

    feat: solarized colorscheme

--- lua/yuu/plugins/colorscheme.lua
+++ lua/yuu/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
 	{
-		"bluz71/vim-nightfly-guicolors",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme nightfly]])
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+			}
 		end,
 	},
 }
